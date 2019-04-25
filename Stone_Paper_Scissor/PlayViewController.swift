98868859d2b56a941fa666c8442027efba2ebb14
sivanpayyadakath <sivan.payyadakath@gmail.com>
2019-04-25 11:06:51 +0900

    design-complete

--- Stone_Paper_Scissor/PlayViewController.swift
+++ Stone_Paper_Scissor/PlayViewController.swift
@@ -10,34 +10,46 @@ import UIKit
 
 class PlayViewController: UIViewController {
 
-    var cnumber: Int!
-    var unumber: Int!
+    var cnumber: Int?
+    var unumber: Int?
+    
+    @IBOutlet var resultView: UIImageView!
+    @IBOutlet var resultText: UITextView!
+    @IBOutlet var uImage: UIImageView!
+    @IBOutlet var cImage: UIImageView!
+    @IBOutlet var playAgain: UIButton!
+    
+    enum Rcase: Int { case rock = 1, paper, scissors}
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(unumber)
-        print(cnumber)
         // Do any additional setup after loading the view.
         switch (unumber, cnumber) {
         case let (unumber, cnumber) where unumber == cnumber:
             print("It's a Tie")
+            resultView.image = UIImage(named: "It's A Tie")
+            resultText.font = .systemFont(ofSize: 50)
+            uImage.image = UIImage(named: "\(Rcase(rawValue: unumber!)!)")
+            cImage.image = UIImage(named: "\(Rcase(rawValue: cnumber!)!)")
         case (1,3), (2, 1), (3,2):
             print("WIN")
+            resultView.image = UIImage(named: "win")
+            cImage.image = UIImage(named: "\(Rcase(rawValue: cnumber!)!)")
+            uImage.image = UIImage(named: "\(Rcase(rawValue: unumber!)!)")
+            resultText.text = "You WIN"
+            resultText.font = .systemFont(ofSize: 50)
         default:
             print("LOSE")
+            resultView.image = UIImage(named: "lose")
+            resultText.text = "You LOSE"
+            resultText.font = .systemFont(ofSize: 50)
+            cImage.image = UIImage(named: "\(Rcase(rawValue: cnumber!)!)")
+            uImage.image = UIImage(named: "\(Rcase(rawValue: unumber!)!)")
         }
     }
     
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func goBack(){
+        self.dismiss(animated: true, completion: nil)
     }
-    */
 
 }
